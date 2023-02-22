ec95447a1bfdbca7851686bfcf2b9734ce18cc0f
Martin Egert <m.egert82@gmail.com>
2023-02-22 10:43:05 +0100

    added classes in aboutUs.js and deleted redundant code in app.css

--- ngo-bike/src/AboutUs.js
+++ ngo-bike/src/AboutUs.js
@@ -1,34 +1,31 @@
 import React, {useState} from "react";
 
-let rotations = null;
-
 export default function AboutUs() {
     const bubblePositions = [
         {left: "300px", top: "0px"}, 
         {left: "600px", top: "300px"},
         {left: "300px", top: "600px"},
         {left: "0px", top: "300px"}
-    ]
+    ];
     
-    const [infoRotation, setInfoRotation] = useState("about-bubble-rotator-still");
+    const [infoRotation, setInfoRotation] = useState("");
     const [posBubble1, setPosBubble1] = useState(bubblePositions[0]);
     const [posBubble2, setPosBubble2] = useState(bubblePositions[1]);
     const [posBubble3, setPosBubble3] = useState(bubblePositions[2]);
     const [posBubble4, setPosBubble4] = useState(bubblePositions[3]);
-    const [bubbleRotation, setBubbleRotation] = useState("about-bubble");
+    const [bubbleRotation, setBubbleRotation] = useState("");
 
-    console.log(posBubble1 === bubblePositions[0])
+    console.log(posBubble1 === bubblePositions[0]);
 
     function handleBubbleClick(id, position) {
-        rotations += 1;
         position.left === "600px" ? rotateCcw() : 
             position.top === "600px" ? rotateHalf() : 
             position.left === "0px" ? rotateQuarter() :
-            setInfoRotation("about-bubble-rotator-still");
+            setInfoRotation("");
 
         setTimeout(() => {
-            setInfoRotation("about-bubble-rotator-still");
-            setBubbleRotation("about-bubble");
+            setInfoRotation("");
+            setBubbleRotation("");
         }, 1000);
     };
     function rotateCcw() {
@@ -67,11 +64,11 @@ export default function AboutUs() {
         <section id="about" className="about">
             <div id="about-info-container">
                 <div id="about-info-textfield"></div>
-                <div id={infoRotation}>
-                    <div className={bubbleRotation} id="bubble1" onClick={e => handleBubbleClick(e.target.id.split("")[6], e.target.style)} style={posBubble1}>some text for testing purposes</div>
-                    <div className={bubbleRotation} id="bubble2" onClick={e => handleBubbleClick(e.target.id.split("")[6], e.target.style)} style={posBubble2}>2</div>
-                    <div className={bubbleRotation} id="bubble3" onClick={e => handleBubbleClick(e.target.id.split("")[6], e.target.style)} style={posBubble3}>3</div>
-                    <div className={bubbleRotation} id="bubble4" onClick={e => handleBubbleClick(e.target.id.split("")[6], e.target.style)} style={posBubble4}>4</div>
+                <div id={infoRotation} className="about-bubble-rotator">
+                    <div className={`${bubbleRotation} about-bubble`} id="bubble1" onClick={e => handleBubbleClick(e.target.id.split("")[6], e.target.style)} style={posBubble1}>some text for testing purposes</div>
+                    <div className={`${bubbleRotation} about-bubble`} id="bubble2" onClick={e => handleBubbleClick(e.target.id.split("")[6], e.target.style)} style={posBubble2}>2</div>
+                    <div className={`${bubbleRotation} about-bubble`} id="bubble3" onClick={e => handleBubbleClick(e.target.id.split("")[6], e.target.style)} style={posBubble3}>3</div>
+                    <div className={`${bubbleRotation} about-bubble`} id="bubble4" onClick={e => handleBubbleClick(e.target.id.split("")[6], e.target.style)} style={posBubble4}>4</div>
                 </div>
             </div>
         </section>
